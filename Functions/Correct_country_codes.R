f709c465e4155db4d2320dd48f9ecf046211c53b
Peter Giovannini <peter.giovannini@gmail.com>
2025-05-19 11:38:11 +0200

    Update Correct_country_codes.R
    Added indentation for readability

--- Functions/Correct_country_codes.R
+++ Functions/Correct_country_codes.R
@@ -2,14 +2,14 @@
 # PG: # PG XAE is CIMMYT and XAM is IRRI as origin of the material, it should not changed to country, ANT is BES
 # Decide what to do with one code equal to multiple current countries
 correct_country_codes <- function(df, col = 'ORIGCTY'){
-df[col][df[col] == "ANT"] <- "BES"
-df[col][df[col] == "ZAR"] <- "COD"
-df[col][df[col] == "ROM"] <- "ROU"
-df[col][df[col] == "BYS"] <- "BLR"
-df[col][df[col] == "SCG"] <- "SER, MNE"
-df[col][df[col] == "YUG"] <- "SLO, HRV, BIH, SRB, MNE, MKD, XKX"
-df[col][df[col] == "CSK"] <- "CZE, SVK"
-df[col][df[col] == "SUN"] <- "RUS"
-df[col][df[col] == "Pak"] <- "PAK"
-return(df)
+  df[col][df[col] == "ANT"] <- "BES"
+  df[col][df[col] == "ZAR"] <- "COD"
+  df[col][df[col] == "ROM"] <- "ROU"
+  df[col][df[col] == "BYS"] <- "BLR"
+  df[col][df[col] == "SCG"] <- "SER, MNE"
+  df[col][df[col] == "YUG"] <- "SLO, HRV, BIH, SRB, MNE, MKD, XKX"
+  df[col][df[col] == "CSK"] <- "CZE, SVK"
+  df[col][df[col] == "SUN"] <- "RUS"
+  df[col][df[col] == "Pak"] <- "PAK"
+  return(df)
 }
