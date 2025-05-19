30892410e7816837195e27380ca05325766a2ed6
Peter Giovannini <peter.giovannini@gmail.com>
2025-05-19 11:36:48 +0200

    Update Assign_annex1_status.R
    Added indentation for readability

--- Functions/Assign_annex1_status.R
+++ Functions/Assign_annex1_status.R
@@ -1,3 +1,4 @@
+# ASSIGN ANNEX 1 DO THIS STEPS ONLY AFTER TAXA STANDARIXATION
 #### Install packages ####
 # tidyverse already include tidyr , dplyr, readr, magrittr, stringr, readxl
 install.packages("tidyverse")
@@ -6,23 +7,22 @@ library(readxl)
 # one would need to change the path for the file contaiing the list of Petota and Melongena species
 #function taking a dataframe including a column taxa names and returning TRUE/FALSE 
 assign_annex1status = function(df, standardize_taxa = 'Standardized_taxa') {
-df <- df %>%
-   mutate(GENUS   = word(!!sym(standardize_taxa), 1),  # Extract the first word (genus)
+  df <- df %>%
+    mutate(GENUS   = word(!!sym(standardize_taxa), 1),  # Extract the first word (genus)
            SPECIES = word(!!sym(standardize_taxa), 2))  # Extract the second word (species)
 
-df$GENUS_SPECIES <- trimws(paste(df$GENUS, df$SPECIES))
+  df$GENUS_SPECIES <- trimws(paste(df$GENUS, df$SPECIES))
 
-# ASSIGN ANNEX 1 DO THIS STEPS ONLY AFTER TAXA STANDARIXATION
-Genus_annex1_food = c('Hordeum', 'Ipomoea', 'Lathyrus', 'Lens', 'Malus', 'Musa', 'Oryza', 'Pennisetum', 'Phaseolus', 'Pisum', 
+  Genus_annex1_food = c('Hordeum', 'Ipomoea', 'Lathyrus', 'Lens', 'Malus', 'Musa', 'Oryza', 'Pennisetum', 'Phaseolus', 'Pisum', 
                 'Secale', 'Sorghum', 'Triticosecale', 'Triticum', 'Aegilops' , 'Agropyron', 'Elymus', 'Secale', 'Vicia', 'Vigna' , 'Zea',
                 'Asparagus' , 'Avena' , 'Beta' , 'Brassica' , 'Armoracia' , 'Barbarea' , 'Camelina' , 'Crambe' , 'Diplotaxis', 
                 'Eruca', 'Isatis' , 'Lepidium', 'Raphanobrassica', 'Raphanus', 'Rorippa', 'Sinapis' , 'Cajanus' , 'Cicer',
                 'Citrus' , 'Cocos' , 'Colocasia' , 'Xanthosoma' , 'Daucus' , 'Dioscorea' , 'Eleusine' , 'Fragaria' , 'Helianthus')
-species_annex1_food = c('Artocarpus altilis' , 'Manihot esculenta' )
+  species_annex1_food = c('Artocarpus altilis' , 'Manihot esculenta' )
 
-# check against GRIN_taxonomy
-# generate a list of synonims from GRIN taxonomy to check against
-species_annex1_forages = c('Astragalus chinensis', 'Astragaslus cicer' , 'Astragalus arenarius' , 'Canavalia ensiformis' , 
+  # check against GRIN_taxonomy
+  # generate a list of synonims from GRIN taxonomy to check against
+  species_annex1_forages = c('Astragalus chinensis', 'Astragaslus cicer' , 'Astragalus arenarius' , 'Canavalia ensiformis' , 
                            'Coronilla varia' , 'Hedysarum coronarium' , 'Lathyrus cicera' , 'Lathyrus ciliolatus' , 'Lathyrus hirsutus', 
                            'Lathyrus ochrus' , 'Lathyrus odoratus' , 'Lathyrus sativus', 'Lespedeza cuneata' , 'Lespedeza striata' , 
                            'Lespedeza stipulacea' , 'Lotus corniculatus', 'Lotus subbiflorus' , 'Lotus uliginosus' , 
@@ -34,39 +34,39 @@ species_annex1_forages = c('Astragalus chinensis', 'Astragaslus cicer' , 'Astrag
                             'Trifolium arvense', 'Trifolium agrocicerum', 'Trifolium hybridum', 'Trifolium incarnatum', 'Trifolium pratense', 'Trifolium repens', 'Trifolium', 'Trifolium',
                             'Trifolium resupinatum', 'Trifolium rueppellianum', 'Trifolium semipilosum', 'Trifolium subterraneum', 'Trifolium vesiculosum')
 
-#Aegilops was included as assuming Triticum et al. includes it
-exclude = c('Lepidium meyenii' , 'Musa textilis' , 'Phaseolus polyanthus', 'Phaseolus dumosus' ,'Zea perennis' , 'Zea diploperennis' , 'Zea luxurians', 'Solanum phureja')
-# potato Section tuberosa included, except Solanum phureja.
-section_Petota <- read_excel("../data_6/processing/Solanum_section_Petota_Species_GRIN-Global.xlsx")
-# Split the "Name" column into "genus" and "species" 
-section_Petota <- section_Petota %>%
-  mutate(GENUS   = word(Name, 1),  # Extract the first word (genus)
-         SPECIES = word(Name, 2))  # Extract the second word (species)
+  #Aegilops was included as assuming Triticum et al. includes it
+  exclude = c('Lepidium meyenii' , 'Musa textilis' , 'Phaseolus polyanthus', 'Phaseolus dumosus' ,'Zea perennis' , 'Zea diploperennis' , 'Zea luxurians', 'Solanum phureja')
+  # potato Section tuberosa included, except Solanum phureja.
+  section_Petota <- read_excel("../data_6/processing/Solanum_section_Petota_Species_GRIN-Global.xlsx")
+  # Split the "Name" column into "genus" and "species" 
+  section_Petota <- section_Petota %>%
+    mutate(GENUS   = word(Name, 1),  # Extract the first word (genus)
+           SPECIES = word(Name, 2))  # Extract the second word (species)
 
-Petota_species = as.list(trimws(section_Petota$SPECIES))
+  Petota_species = as.list(trimws(section_Petota$SPECIES))
 
-#eggplant Section melongena included species list in file Section_Melongena_GRIN-Global.xlsx
-section_Melongena <- read_excel("../data_6/processing/Section_Melongena_Species_GRIN-Global.xlsx")
-# Split the "fullSciName" column into "genus" and "species" without removing "fullSciName"
-section_Melongena <- section_Melongena %>%
-  mutate(GENUS   = word(Name, 1),  # Extract the first word (genus)
-         SPECIES = word(Name, 2))  # Extract the second word (species)
+  #eggplant Section melongena included species list in file Section_Melongena_GRIN-Global.xlsx
+  section_Melongena <- read_excel("../data_6/processing/Section_Melongena_Species_GRIN-Global.xlsx")
+  # Split the "fullSciName" column into "genus" and "species" without removing "fullSciName"
+  section_Melongena <- section_Melongena %>%
+    mutate(GENUS   = word(Name, 1),  # Extract the first word (genus)
+           SPECIES = word(Name, 2))  # Extract the second word (species)
 
-section_Melongena = as.list(trimws(section_Melongena$SPECIES))
+  section_Melongena = as.list(trimws(section_Melongena$SPECIES))
 
-#species annex 1 here make list
-# assign based on the list above
+  # start with all FALSE
+  df$Annex1 = FALSE
+  df$Annex1 <- ifelse(df$GENUS %in% Genus_annex1_food, TRUE, df$Annex1) # if genus is in list genus annex 1 = True
+  df$Annex1 <- ifelse(df$GENUS_SPECIES %in% species_annex1_forages, TRUE, df$Annex1) 
+  
+  # check if species is Potato or its CWRs
+  df$Annex1 <- ifelse( (df$GENUS == 'Solanum') & (df$SPECIES %in% Petota_species), TRUE, df$Annex1) 
+  
+  # check if species is Eggplants or its CWRs
+  df$Annex1 <- ifelse( (df$GENUS == 'Solanum') & (df$SPECIES %in% section_Melongena), TRUE, df$Annex1) 
+  
+  # exclude some species
+  df$Annex1 <- ifelse(df$GENUS_SPECIES %in% exclude , FALSE, df$Annex1) 
 
-# start with all FALSE
-df$Annex1 = FALSE
-df$Annex1 <- ifelse(df$GENUS %in% Genus_annex1_food, TRUE, df$Annex1) # if genus is in list genus annex 1 = True
-df$Annex1 <- ifelse(df$GENUS_SPECIES %in% species_annex1_forages, TRUE, df$Annex1) 
-# check if species is Potato or its CWRs
-df$Annex1 <- ifelse( (df$GENUS == 'Solanum') & (df$SPECIES %in% Petota_species), TRUE, df$Annex1) 
-# check if species is Eggplants or its CWRs
-df$Annex1 <- ifelse( (df$GENUS == 'Solanum') & (df$SPECIES %in% section_Melongena), TRUE, df$Annex1) 
-# exclude some species
-df$Annex1 <- ifelse(df$GENUS_SPECIES %in% exclude , FALSE, df$Annex1) 
-
-return(df)
+  return(df)
 } # end of function
