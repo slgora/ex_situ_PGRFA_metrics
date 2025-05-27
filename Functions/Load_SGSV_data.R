c3fb676086e827ee524529a99f2e9529b1c5f4dd
Peter Giovannini <peter.giovannini@gmail.com>
2025-05-27 10:01:22 +0200

    Update Load_SGSV_data.R
    fixed errors after testing it

--- Functions/Load_SGSV_data.R
+++ Functions/Load_SGSV_data.R
@@ -1,14 +1,15 @@
 # library(readxl) us needed
-function = load_SGSV_data(filepath){
+load_SGSV_data = function(filepath){
   SGSV_allcrops <- read_excel(filepath)
   # Change column names
   colnames(SGSV_allcrops) <- c( "source","instName","INSTCODE","instAcronym","ACCENUMB",
-                              "fullTaxa","GENUS_SPECIES","origctyFullName") #origCtyFullName
+                                "fullTaxa","GENUS_SPECIES","origctyFullName") #origCtyFullName
   ## Keep fields
   SGSV_allcrops <- subset(SGSV_allcrops, select = c(INSTCODE, instAcronym, 
-                                                  ACCENUMB, fullTaxa, fullTaxa1, 
-                                                  origctyFullName))
+                                                    ACCENUMB, fullTaxa, GENUS_SPECIES, 
+                                                    origctyFullName))
   # create SPECIES and GENUS field
-  SGSV_allcrops  <- SGSV_allcrops  %>% separate(fullTaxa1, c('GENUS', 'SPECIES'))
+  SGSV_allcrops  <- SGSV_allcrops  %>% separate(GENUS_SPECIES, c('GENUS', 'SPECIES'))
   return(SGSV_allcrops)
 }
+
