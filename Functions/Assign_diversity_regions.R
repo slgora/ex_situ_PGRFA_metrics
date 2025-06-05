6e819403bcfae87a842b5958a0ecb49af139a74a
Peter Giovannini <peter.giovannini@gmail.com>
2025-06-05 17:37:53 +0200

    Update Assign_diversity_regions.R
    deleted condition in  genera_to_classify,

--- Functions/Assign_diversity_regions.R
+++ Functions/Assign_diversity_regions.R
@@ -14,10 +14,11 @@ assign_diversity_regions = function(df, crops, countries_in_regions) {
   # create and assign values to column of primary and secondary diversity regions
   df$Primary_diversity_region = NA
   df$secondary_diversity_region = NA
-  df <- df %>%
-    mutate(Primary_diversity_region = ifelse(GENUS %in% genera_to_classify, primary_regions[GENUS], NA)) %>%
-    mutate(secondary_diversity_region = ifelse(GENUS %in% genera_to_classify, secondary_regions[GENUS], NA)) 
 
+  df <- df %>%
+     mutate(Primary_diversity_region = primary_regions[GENUS]) %>%
+     mutate(secondary_diversity_region = secondary_regions[GENUS])
+  
   # create dictionary Country code -> region (following formatting in regions of diversity)
   country_regions_dictionary <- setNames(countries_in_regions$PlantsThatFeedTheWorld_Region_new, countries_in_regions$Country_code )
   
