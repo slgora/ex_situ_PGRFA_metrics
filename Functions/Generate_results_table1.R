e188be51a764bc2b7030350efa75377b20f9ea92
Sarah Gora <sarahluciagora@gmail.com>
2025-06-27 17:21:36 -0600

    SG added script for generating table 1 based on format suggestion in Metrics and Data Descriptions file

---
+++ Functions/Generate_results_table1.R
@@ -0,0 +1,65 @@
+## SG add info on function her
+
+## generate metrics table 1 function   
+generate_table1 <- function(tbl_number, summary_df, metrics_guide) {
+  crop_column <- names(summary_df)[1]  # e.g. "cropstrategy"
+  
+  crop_tables <- summary_df[[crop_column]] %>%
+    unique() %>%
+    set_names() %>%
+    map(function(crop) {
+      
+      table1_guide <- metrics_guide %>%
+        filter(`Pertains to Table` == tbl_number)
+      
+      long_metrics <- table1_guide %>%
+        select(
+          `Row in Table`,
+          `Order in table`,
+          `Metric Name in Results Table (or summaries text)`,
+          `Name of Individual Metric Variable`,
+          `Metric Role`
+        ) %>%
+        mutate(
+          raw_value = purrr::map_chr(`Name of Individual Metric Variable`, function(colname) {
+            if (!is.na(colname) && colname %in% names(summary_df)) {
+              val <- summary_df %>%
+                filter(!!sym(crop_column) == crop) %>%
+                pull(colname)
+              if (length(val) > 0 && !is.na(val)) format(val, digits = 3) else ""
+            } else {
+              ""
+            }
+          })
+        )
+      
+      # Pivot roles into columns based on Row in Table
+      wide <- long_metrics %>%
+        pivot_wider(
+          id_cols = `Row in Table`,
+          names_from = `Metric Role`,
+          values_from = raw_value
+        )
+      
+      # Pull metric labels from 'Value' rows
+      labels <- long_metrics %>%
+        filter(`Metric Role` == "Value") %>%
+        select(`Row in Table`, Metric = `Metric Name in Results Table (or summaries text)`)
+      
+      final <- wide %>%
+        left_join(labels, by = "Row in Table") %>%
+        arrange(`Row in Table`) %>%
+        transmute(
+          Crop = crop,
+          Metric,
+          Value,
+          `Number of countries where significant contributor`,
+          `Evenness of contribution across world regions`,
+          `Estimated international interdependence`
+        )
+      
+      return(final)
+    })
+  
+  return(crop_tables)
+}
