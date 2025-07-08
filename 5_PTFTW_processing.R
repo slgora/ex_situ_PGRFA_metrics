705a05a5589f247bd957c1860b5a68e19b40a329
Sarah Gora <sarahluciagora@gmail.com>
2025-07-08 16:00:32 -0600

    staging file for PTFTW metrics script

---
+++ 5_PTFTW_processing.R
@@ -0,0 +1,6 @@
+################ PTFTW data ############################################################################
+## note: will need to update local paths within function to work properly
+source("Functions/load_PTFTW_dataset.R")
+PTFTW = process_ptftw_indicator_data(output_xlsx = "../../Data_processing/1_merge_data/2025_07_07/PTFTW_processed.xlsx")
+
+PTFTW = process_ptftw_indicator_data(output_xlsx = "G:/.shortcut-targets-by-id/1GnMqdK_h04rDh_GYxxYBWiyuGZFSN2GZ/GCCS metrics project shared folder/Data_processing/1_merge_data/2025_07_07/PTFTW_processed.xlsx")
