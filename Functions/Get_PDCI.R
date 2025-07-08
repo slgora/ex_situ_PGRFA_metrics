d07b337b607c44d0d2121af40b15f85fb2383a3a
Sarah Gora <sarahluciagora@gmail.com>
2025-07-08 13:27:21 -0600

    Updated get_PDCI function

--- Functions/Get_PDCI.R
+++ Functions/Get_PDCI.R
@@ -1,136 +1,78 @@
-## Need to write up the background info for this function etc.
 
-#PDCI function takes into account 28 columns (genesys):
-# Column name synonyms:
+# PDCI function
 
-# `GENUS` = genus
-# `SPECIES`= species
-- `SPAUTHOR` = spAuthor
-- `SUBTAXA`=subTaxa
-- `SUBTAUTHOR`= subTAuthor
-# `CROPNAME`= cropName
-- `ACQDATE`= acqDate
-# `SAMPSTAT`= sampStat, 
-- `DONORCODE`= donorCode
-- `DONORNAME`= donorName
-- `DONORNUMB`=  donorNumb
-- `OTHERNUMB`= otherNumb
-# `DUPLSITE` = duplSite
-# `STORAGE` = storage
-- `ACCEURL`= acceUrl
-# `MLSSTAT`= mlsStat
-# `ORIGCTY`= origCty
-- `COLLSITE`= collSite
-#`DECLATITUDE` = latitude
-# `DECLONGITUDE`= longitude
-- `ELEVATION` = elevation
-- `COLLDATE`= collDate
-- `BREDCODE`= bredCode
-- `ANCEST`= ancest
-- `COLLSRC`= collSrc
-# `ACCENAME`= acceNumb
-- `COLLNUMB`= collNumb
-- `COLLCODE`= collCode
-
+## Note: PDCI function takes into account 28 columns of passport data
+# `GENUS`,`SPECIES`,`SPAUTHOR`,SUBTAXA`, SUBTAUTHOR`,`CROPNAME`,`ACQDATE`,`SAMPSTAT`,
+# `DONORCODE`,`DONORNAME`,`DONORNUMB`,`OTHERNUMB`,`DUPLSITE`,`STORAGE`,`ACCEURL`,`MLSSTAT`= mlsStat
+# `ORIGCTY`,`COLLSITE`,`DECLATITUDE`, `DECLONGITUDE`,`ELEVATION`, `COLLDATE`
+# `BREDCODE`, `ANCEST`, `COLLSRC`, `ACCENAME`,`COLLNUMB`,`COLLCODE`
 
 get_PDCI <- function(df) {
   # Initialize PDCI column with 0
   df$PDCI <- 0
   
-  # Function to print debug information
-  print_debug_info <- function(df, step) {
-    cat(paste("After step:", step, "\n"))
-    print(head(df[, c("genus", "species", "storage", "origCty", "sampStat", "acceUrl", "mlsStat", "ancest", "latitude", "longitude", "collDate", "bredCode", "PDCI")], 5))
-  }
-  
-  # Print initial state of the data
-  print_debug_info(df, "initial")
-  
   # Apply conditions and update PDCI
-  df$PDCI <- ifelse(!is.na(df$genus), df$PDCI + 120, df$PDCI)
-  print_debug_info(df, "genus")
+  df$PDCI <- ifelse(!is.na(df$GENUS), df$PDCI + 120, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$GENUS) & !is.na(df$SPECIES) & !df$SPECIES %in% c('sp', 'sp.', 'spp.', 'spp'), df$PDCI + 80, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$GENUS) & !is.na(df$SPECIES) & !is.na(df$SPAUTHOR), df$PDCI + 5, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$GENUS) & !is.na(df$SPECIES) & !is.na(df$SUBTAXA), df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$GENUS) & !is.na(df$SPECIES) & !is.na(df$SUBTAXA) & !is.na(df$SUBTAUTHOR), df$PDCI + 5, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$CROPNAME), df$PDCI + 45, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ACQDATE), df$PDCI + 10, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$SAMPSTAT), df$PDCI + 80, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DONORCODE), df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DONORNAME) & is.na(df$DONORCODE), df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DONORNUMB) & (!is.na(df$DONORCODE) | !is.na(df$DONORNAME)), df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DONORNUMB) & is.na(df$DONORCODE) & is.na(df$DONORNAME), df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$OTHERNUMB), df$PDCI + 35, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DUPLSITE), df$PDCI + 30, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$STORAGE), df$PDCI + 15, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ACCEURL), df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$MLSSTAT), df$PDCI + 15, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$genus) & !is.na(df$species) & !df$species %in% c('sp', 'sp.', 'spp.', 'spp'), df$PDCI + 80, df$PDCI)
-  print_debug_info(df, "genus and species")
+  df$PDCI <- ifelse(!is.na(df$ORIGCTY) & df$SAMPSTAT %/% 100 < 4, df$PDCI + 80, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ORIGCTY) & (df$SAMPSTAT %/% 100 >= 4 | is.na(df$SAMPSTAT)), df$PDCI + 40, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$genus) & !is.na(df$species) & !is.na(df$spAuthor), df$PDCI + 5, df$PDCI)
-  print_debug_info(df, "species author")
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & df$SAMPSTAT %/% 100 < 3 & (is.na(df$DECLATITUDE) | is.na(df$DECLONGITUDE)), df$PDCI + 70, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & df$SAMPSTAT %/% 100 == 3 & (is.na(df$DECLATITUDE) | is.na(df$DECLONGITUDE)), df$PDCI + 45, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$genus) & !is.na(df$species) & !is.na(df$subTaxa), df$PDCI + 40, df$PDCI)
-  print_debug_info(df, "subTaxa")
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & df$SAMPSTAT %/% 100 < 3 & !is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE), df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & df$SAMPSTAT %/% 100 == 3 & !is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE), df$PDCI + 15, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & (df$SAMPSTAT %/% 100 == 9 | is.na(df$SAMPSTAT)) & !is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE), df$PDCI + 10, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSITE) & (df$SAMPSTAT %/% 100 == 9 | is.na(df$SAMPSTAT)) & (is.na(df$DECLATITUDE) | is.na(df$DECLONGITUDE)), df$PDCI + 20, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$genus) & !is.na(df$species) & !is.na(df$subTaxa) & !is.na(df$subTAuthor), df$PDCI + 5, df$PDCI)
-  print_debug_info(df, "subTaxa author")
+  df$PDCI <- ifelse(!is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE) & df$SAMPSTAT %/% 100 < 3, df$PDCI + 120, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 80, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$DECLATITUDE) & !is.na(df$DECLONGITUDE) & (df$SAMPSTAT %/% 100 == 9 | is.na(df$SAMPSTAT)), df$PDCI + 30, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$cropName), df$PDCI + 45, df$PDCI)
-  print_debug_info(df, "cropName")
+  df$PDCI <- ifelse(!is.na(df$ELEVATION) & df$SAMPSTAT %/% 100 < 3, df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ELEVATION) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 15, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ELEVATION) & (df$SAMPSTAT %/% 100 == 9 | is.na(df$SAMPSTAT)), df$PDCI + 5, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$acqDate), df$PDCI + 10, df$PDCI)
-  print_debug_info(df, "acqDate")
+  df$PDCI <- ifelse(!is.na(df$COLLDATE) & df$SAMPSTAT %/% 100 < 4, df$PDCI + 30, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ANCEST) & (df$SAMPSTAT %/% 100 > 5 | is.na(df$SAMPSTAT)), df$PDCI + 40, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$sampStat), df$PDCI + 80, df$PDCI)
-  print_debug_info(df, "sampStat")
+  df$PDCI <- ifelse(!is.na(df$COLLSRC) & df$SAMPSTAT %/% 100 < 3, df$PDCI + 30, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSRC) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 50, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSRC) & df$SAMPSTAT %/% 100 == 4, df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSRC) & df$SAMPSTAT %/% 100 == 5, df$PDCI + 20, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLSRC) & (df$SAMPSTAT %/% 100 > 5 | is.na(df$SAMPSTAT)), df$PDCI + 25, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$donorCode), df$PDCI + 40, df$PDCI)
-  print_debug_info(df, "donorCode")
+  df$PDCI <- ifelse(!is.na(df$ACCENAME) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 50, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ACCENAME) & df$SAMPSTAT %/% 100 == 4, df$PDCI + 80, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ACCENAME) & df$SAMPSTAT %/% 100 == 5, df$PDCI + 160, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$ACCENAME) & (df$SAMPSTAT %/% 100 > 5 | is.na(df$SAMPSTAT)), df$PDCI + 40, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$donorName) & is.na(df$donorCode), df$PDCI + 20, df$PDCI)
-  print_debug_info(df, "donorName without donorCode")
+  df$PDCI <- ifelse(!is.na(df$COLLNUMB) & df$SAMPSTAT %/% 100 < 3, df$PDCI + 60, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLNUMB) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLNUMB) & (df$SAMPSTAT %/% 100 > 5 | is.na(df$SAMPSTAT)), df$PDCI + 20, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$donorNumb) & (!is.na(df$donorCode) | !is.na(df$donorName)), df$PDCI + 40, df$PDCI)
-  print_debug_info(df, "donorNumb with donorCode or donorName")
+  df$PDCI <- ifelse(!is.na(df$COLLCODE) & df$SAMPSTAT %/% 100 < 3, df$PDCI + 40, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLCODE) & df$SAMPSTAT %/% 100 == 3, df$PDCI + 30, df$PDCI)
+  df$PDCI <- ifelse(!is.na(df$COLLCODE) & (df$SAMPSTAT %/% 100 > 5 | is.na(df$SAMPSTAT)), df$PDCI + 20, df$PDCI)
   
-  df$PDCI <- ifelse(!is.na(df$donorNumb) & is.na(df$donorCode) & is.na(df$donorName), df$PDCI + 20, df$PDCI)
-  print_debug_info(df, "donorNumb without donorCode or donorName")
+  # Normalize PDCI by dividing by 100
+  df$PDCI <- df$PDCI / 100
   
-  df$PDCI <- ifelse(!is.na(df$otherNumb), df$PDCI + 35, df$PDCI)
-  print_debug_info(df, "otherNumb")
-  
-  df$PDCI <- ifelse(!is.na(df$duplSite), df$PDCI + 30, df$PDCI)
-  print_debug_info(df, "duplSite")
-  
-  df$PDCI <- ifelse(!is.na(df$storage), df$PDCI + 15, df$PDCI)
-  print_debug_info(df, "storage")
-  
-  df$PDCI <- ifelse(!is.na(df$acceUrl), df$PDCI + 40, df$PDCI)
-  print_debug_info(df, "acceUrl")
-  
-  df$PDCI <- ifelse(!is.na(df$mlsStat), df$PDCI + 15, df$PDCI)
-  print_debug_info(df, "mlsStat")
-  
-  # Handle origCty and sampStat conditions, considering NA values for sampStat
-  df$PDCI <- ifelse(!is.na(df$origCty) & (is.na(df$sampStat) | df$sampStat %/% 100 < 4), df$PDCI + 80, df$PDCI)
-  print_debug_info(df, "origCty with sampStat < 4")
-  
-  df$PDCI <- ifelse(!is.na(df$origCty) & !is.na(df$sampStat) & df$sampStat %/% 100 >= 4, df$PDCI + 40, df$PDCI)
-  print_debug_info(df, "origCty with sampStat >= 4")
-  
-  # Check latitude and longitude conditions
-  df$PDCI <- ifelse(!is.na(df$latitude) & !is.na(df$longitude), df$PDCI + 120, df$PDCI)
-  print_debug_info(df, "latitude and longitude")
-  
-  # Check elevation condition
-  df$PDCI <- ifelse(!is.na(df$elevation), df$PDCI + 20, df$PDCI)
-  print_debug_info(df, "elevation")
-  
-  # Check collection date condition
-  df$PDCI <- ifelse(!is.na(df$collDate), df$PDCI + 30, df$PDCI)
-  print_debug_info(df, "collDate")
-  
-  # Check breeding code condition
-  df$PDCI <- ifelse(!is.na(df$bredCode), df$PDCI + 110, df$PDCI)
-  print_debug_info(df, "bredCode")
-  
-  # Check ancestry condition
-  df$PDCI <- ifelse(!is.na(df$ancest), df$PDCI + 150, df$PDCI)
-  print_debug_info(df, "ancest")
-  
-  # Normalize PDCI by dividing by 100 
-  df$PDCI <- df$PDCI / 100 
-  
-  # Ensure PDCI is between 0 and 10
-  df$PDCI[is.na(df$PDCI)] <- 0
-  df$PDCI <- pmin(pmax(df$PDCI, 0), 10)
-  
-  return(df) 
-} 
+  return(df)
+}
