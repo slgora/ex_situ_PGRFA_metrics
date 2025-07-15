f160f50add6304bd108308b5bad47a764d22f499
Sarah Gora <sarahluciagora@gmail.com>
2025-07-15 11:19:26 -0600

    Added Table 7 script

--- 6_generate_tables.R
+++ 6_generate_tables.R
@@ -1,5 +1,4 @@
 # testing script for building out results tables 
-
 library(readr)
 
 # Read in Metrics_and_data_descriptions_table as guide file for table set up
@@ -17,7 +16,7 @@ source("Functions/Generate_results_table1.R")
 # source("Functions/Generate_results_table4.R") #didnt push to github yet
 # source("Functions/Generate_results_table5.R") #didnt push to github yet
 # source("Functions/Generate_results_table6.R") #didnt push to github yet
-# source("Functions/Generate_results_table7.R") #didnt push to github yet
+source("Functions/Generate_results_table7.R")
 
 # note: Create a folder for date of run to export results tables 
 
@@ -27,7 +26,19 @@ filtered_guide <- metrics_guide %>% filter(`Pertains to Table` == 1)
 # Fun function to generate table 1
 table1_by_crop <- generate_table1(1, PTFTW_metrics, filtered_guide)
 # Export all crop tables into one Excel file with each crop as a tab
-write_xlsx(table1_by_crop, path = "../../Data_processing/6_generate_tables/2025_07_14/Table1_all_crops.xlsx")
+write_xlsx(table1_by_crop, path = "../../Data_processing/6_generate_tables/2025_07_15/Table1_all_crops.xlsx")
 
 
-# note: script for Tables 2- Table 7 not pushed to Github yet
+# note: script for Tables 2-6 not pushed to Github yet
+
+
+# ---------- Table 7 ------------
+# Create list of metrics for table 7
+metric_dfs <- list(
+  PTFTW_metrics      = PTFTW_metrics,
+  transfer_metrics  = transfer_metrics
+)
+# Run function to generate table 7
+table7_by_crop <- generate_table7(metrics_guide, metric_dfs)
+# Export all crop tables into one Excel file with each crop as a tab
+write_xlsx(table7_by_crop, path = "../../Data_processing/6_generate_tables/2025_07_15/Table7_all_crops.xlsx")
