87437b2684894fd07e06ca91e0a81f3d26852ac1
Sarah Gora <sarahluciagora@gmail.com>
2025-07-15 14:26:42 -0600

    Added Table 2 script
    Added Table 2 script and modified read in of metrics file as a list so can call easily to tables.

--- 6_generate_tables.R
+++ 6_generate_tables.R
@@ -3,15 +3,19 @@ library(readr)
 
 # Read in Metrics_and_data_descriptions_table as guide file for table set up
 metrics_guide <- read_excel("../../Data_processing/Metrics_and_data_descriptions_table.xlsx")
-
-# Read in metrics calculated
+# Read in PTFTW and transfer metrics calculated
 PTFTW_metrics <- read_excel("../../Data_processing/5_PTFTW_processing_and_metrics/2025_07_14/PTFTW_metrics.xlsx")
 transfer_metrics <- read_excel("../../Data_processing/5_PTFTW_processing_and_metrics/2025_07_14/transfers_metrics_2015_2021.xlsx")
-all_metrics <- read_excel("../../Data_processing/4_estimate_metrics/2025_07_15/all_metrics_summary.xlsx")
+# Read in all other metrics file
+all_metrics <- "../../Data_processing/4_estimate_metrics/2025_07_15/all_metrics_summary.xlsx"
+sheet_names <- getSheetNames(all_metrics)
+all_metrics <- setNames(      #Read all sheets into a named list so can call to tables
+  lapply(sheet_names, function(s) read.xlsx(all_metrics, sheet = s)),
+  sheet_names)
 
 # Source function(s)
 source("Functions/Generate_results_table1.R")
-# source("Functions/Generate_results_table2.R") #didnt push to github yet
+source("Functions/Generate_results_table2.R")
 # source("Functions/Generate_results_table3.R") #didnt push to github yet
 # source("Functions/Generate_results_table4.R") #didnt push to github yet
 # source("Functions/Generate_results_table5.R") #didnt push to github yet
@@ -28,8 +32,16 @@ table1_by_crop <- generate_table1(1, PTFTW_metrics, filtered_guide)
 # Export all crop tables into one Excel file with each crop as a tab
 write_xlsx(table1_by_crop, path = "../../Data_processing/6_generate_tables/2025_07_15/Table1_all_crops.xlsx")
 
+# ---------- Table 2 ------------
+# Extract metric needed for Table 2
+institution_accessions_summary <- all_metrics$institution_accessions_summary
+# Run function to generate table 2
+table2_by_crop <- generate_table2(institution_accessions_summary)
+# Export all crop tables into one Excel file with each crop as a tab
+write_xlsx(table2_by_crop, path = "../../Data_processing/6_generate_tables/2025_07_15/Table2_all_crops.xlsx")
 
-# note: script for Tables 2-6 not pushed to Github yet
+
+# note: script for Tables 3-6 not pushed to Github yet
 
 
 # ---------- Table 7 ------------
