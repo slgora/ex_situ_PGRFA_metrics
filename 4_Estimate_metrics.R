079756b8d779a903eabb3cd51eb63397796bc0b9
Sarah Gora <sarahluciagora@gmail.com>
2025-07-15 10:47:56 -0600

    Compile metrics into one output
    Added script to compile metrics into one list and save as an excel file. Deleted individual metric calculation output files.

--- 4_Estimate_metrics.R
+++ 4_Estimate_metrics.R
@@ -25,16 +25,11 @@ institute_names_no_syn <- read_excel("../../Data_processing/Support_files/FAO_WI
 
 # 1. Total number of accessions by crop strategy
 accession_by_crop_strategy <- combined_allcrops %>% count(Crop_strategy, name = "accessions_count")
-accession_by_source_crop_strategy <- combined_allcrops %>% count(data_source, Crop_strategy, name = "accessions_count")
-# save results
-write.csv(accession_by_crop_strategy, '../../Data_processing/4_Estimate_metrics/2025_07_14/accession_by_crop_strategy.csv', row.names = FALSE)
-write.csv(accession_by_source_crop_strategy, '../../Data_processing/4_Estimate_metrics/2025_07_14/accession_by_source_crop_strategy.csv', row.names = FALSE)
+accession_by_source <- combined_allcrops %>% count(data_source, Crop_strategy, name = "accessions_count")
 
 # 2. Unique institutions per crop strategy
 unique_institutions <- combined_allcrops %>%
   group_by(Crop_strategy) %>% summarise(unique_instcount = n_distinct(INSTCODE), .groups = "drop")
-# save results
-write.csv(unique_institutions, '../../Data_processing/4_Estimate_metrics/2025_07_14/unique_institutions.csv', row.names = FALSE)
 
 # 3. Wild, Weedy, Landrace, Breeding, Improved, Other, No SAMPSTAT - % summaries
 cwr_metric <- percent_summary(combined_allcrops, Crop_strategy, sum(SAMPSTAT >= 100 & SAMPSTAT < 200, na.rm = TRUE), cwr_total_records, SAMPSTAT100_perc) %>%
@@ -51,14 +46,6 @@ othervar_metric <- percent_summary(combined_allcrops, Crop_strategy, sum(SAMPSTA
   rename(SAMPSTAT999_count = count)
 no_SAMPSTAT_metric <- percent_summary(combined_allcrops, Crop_strategy, sum(is.na(SAMPSTAT)), noSAMPSTAT_total_records, SAMPSTATna_perc)%>%
   rename(SAMPSTATna_count = count)
-# save results
-write.csv(cwr_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/cwr_metric.csv", row.names = FALSE)
-write.csv(weedy_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/weedy_metric.csv", row.names = FALSE)
-write.csv(landrace_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/landrace_metric.csv", row.names = FALSE)
-write.csv(breeding_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/breeding_metric.csv", row.names = FALSE)
-write.csv(improved_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/improved_metric.csv", row.names = FALSE)
-write.csv(othervar_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/othervar_metric.csv", row.names = FALSE)
-write.csv(no_SAMPSTAT_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/no_SAMPSTAT_metric.csv", row.names = FALSE)
 
 # 4. Unique taxa per crop
 unique_taxa <- combined_allcrops %>%
@@ -70,18 +57,12 @@ unique_taxa <- combined_allcrops %>%
     unique_taxa_count = n_distinct(Standardized_taxa),
     .groups = "drop"
   )
-# save results
-unique_taxa <- unique_taxa %>%  # Convert taxa column to semicolon-separated string
-  mutate(unique_taxa = sapply(unique_taxa, function(x) paste(x, collapse = "; ")))
-write.csv(unique_taxa, "../../Data_processing/4_Estimate_metrics/2025_07_14/unique_taxa.csv", row.names = FALSE)
 
 # 5. Number of countries where germplasm collected (excluding certain SAMPSTAT)
 country_count <- combined_allcrops %>%
   filter(!(SAMPSTAT %in% c(400:499, 500, 600))) %>%
   group_by(Crop_strategy) %>%
   summarise(unique_countrycount = n_distinct(ORIGCTY), .groups = "drop")
-# save results
-write.csv(country_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/country_count.csv", row.names = FALSE)
 
 # 6.a and 6.b Accessions from primary & secondary regions of diversity
 primary_region_metric <- combined_allcrops %>%
@@ -92,8 +73,6 @@ primary_region_metric <- combined_allcrops %>%
     primaryregions_total_records,
     isinprimaryregion_perc
   )
-#save results
-write.csv(primary_region_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/primary_region_metric.csv", row.names = FALSE)
 
 # 6.c and 6.d Diversity_regions_metric (primary + secondary regions)
 diversity_regions_metric <- combined_allcrops %>%
@@ -106,8 +85,6 @@ diversity_regions_metric <- combined_allcrops %>%
     isindiversityregions_perc = round(100 * isindiversityregions_count / total_accessions, 2),
     .groups = "drop"
   )
-# save results
-write.csv(diversity_regions_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/diversity_regions_metric.csv", row.names = FALSE)
 
 # 7. accessions by org type,  and MLS accessions for organization type (A15 collection versus non-A15)
 combined_allcrops <- combined_allcrops %>% 
@@ -133,9 +110,6 @@ mls_by_orgtype <- combined_allcrops %>%
     percent_notincludedmls = round(100 * count_notincludedmls / total, 2),
     .groups = "drop"
   )
-#save results
-write.csv(accessions_by_org_type, "../../Data_processing/4_Estimate_metrics/2025_07_14/accessions_by_org_type.csv", row.names = FALSE)
-write.csv(mls_by_orgtype, "../../Data_processing/4_Estimate_metrics/2025_07_14/mls_by_orgtype.csv", row.names = FALSE)
 
 # 8. Accessions in Annex I
 annex1_count <- combined_allcrops %>%
@@ -149,9 +123,6 @@ annex1_perc <- combined_allcrops %>%
     .groups = "drop"
   ) %>%
   mutate(annex1_perc = round((count_includedannex1 / annex1_total_records) * 100, 2))
-# save results
-write.csv(annex1_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/annex1_count.csv", row.names = FALSE)
-write.csv(annex1_perc, "../../Data_processing/4_Estimate_metrics/2025_07_14/annex1_perc.csv", row.names = FALSE)
 
 # 9. Storage type metrics (all, and by term)
 storage_types <- list(
@@ -199,9 +170,6 @@ storage_term_summary <- combined_allcrops %>%
     medterm_storage_perc    = round(100 * medterm_storage_count / total_records, 2),
     shortterm_storage_perc  = round(100 * shortterm_storage_count / total_records, 2)
   )
-# save results
-write.csv(storage_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/storage_summary.csv", row.names = FALSE)
-write.csv(storage_term_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/storage_term_summary.csv", row.names = FALSE)
 
 # 10. Safety duplication percentage of accessions duplicated out of the country in other genebanks (excluding SGSV) calculated only using Genesys data. 
 source("Functions/SD_duplicates_out_country.R") # source function
@@ -222,10 +190,7 @@ sd_outcountry_metric <- genesys %>%
   ) %>%
   arrange(desc(sd_out_country_count)) %>%
   mutate(sd_out_country_perc = 100 * sd_out_country_count / accessions_total)
-
-# Save output to Drive folder, please use subfolder with date of running the script 
 sd_outcountry_metric <- apply(sd_outcountry_metric,2,as.character)
-write.csv(sd_outcountry_metric, '../../Data_processing/4_Estimate_metrics/2025_07_14/sd_outcountry_metric.csv', row.names = FALSE)
 
 # 11. SGSV duplicates
 SGSV_dupl_metric <- SGSV_allcrops %>%
@@ -236,22 +201,16 @@ SGSV_dupl_metric <- SGSV_allcrops %>%
   ) %>%
   mutate(sgsv_dupl_perc = round((sgsv_dupl_count / total_count) * 100, 2)) %>%
   select(Crop_strategy, sgsv_dupl_count, total_count, sgsv_dupl_perc)
-# save results
-write.csv(SGSV_dupl_metric, "../../Data_processing/4_Estimate_metrics/2025_07_14/SGSV_dupl_metric.csv", row.names = FALSE)
 
 # 12. GLIS: # of accessions with DOIs per crop, use data downloaded from GLIS (GLIS_dataset)
 GLIS_dois_count <- GLIS_dataset %>%
   group_by(Crop_strategy) %>%
   summarise(dois = sum(!is.na(DOI) & DOI != ""), .groups = "drop")
-# save results
-write.csv(GLIS_dois_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/GLIS_dois_count.csv", row.names = FALSE)
 
 # 13: GLIS: # of accessions notified as include in MLS (based on GLIS dataset)
 GLIS_MLS_count <- GLIS_dataset %>% 
   group_by(Crop_strategy) %>% 
   summarise(MLS_notified = sum(MLSSTAT, na.rm = TRUE), .groups = "drop")
-# save results
-write.csv(GLIS_MLS_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/GLIS_MLS_count.csv", row.names = FALSE)
 
 # 14. Top institutions holding crop germplasm
 institution_accessions_summary <- combined_allcrops %>%
@@ -277,30 +236,22 @@ institution_accessions_summary <- institution_accessions_summary %>%
       is.na(Institute_name),
       table_INSTCODE_to_name[INSTCODE],
       Institute_name))
-# save results
-write.csv(institution_accessions_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/institution_accessions_summary.csv", row.names = FALSE)
 
 # 15. Number of unique taxa listed in BGCI data metric (BGCI dataset)
 BGCI_taxa_count <- BGCI_allcrops %>%
   filter(!is.na(Standardized_taxa)) %>%
   distinct(Crop_strategy, Standardized_taxa) %>%
   count(Crop_strategy, name = "bgci_unique_taxa_count")
-# save results
-write.csv(BGCI_taxa_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/BGCI_taxa_count.csv", row.names = FALSE)
 
 # 16. Number of unique institutions holding crop germplasm (BGCI dataset)
 BGCI_inst_count <- BGCI_allcrops %>%
   filter(!is.na(ex_situ_site_gardenSearch_ID)) %>%
   distinct(Crop_strategy, ex_situ_site_gardenSearch_ID) %>%
   count(Crop_strategy, name = "unique_inst_count")
-# save results
-write.csv(BGCI_inst_count, "../../Data_processing/4_Estimate_metrics/2025_07_14/BGCI_inst_count.csv", row.names = FALSE)
 
 # 17. Regeneration metrics (based on WIEWS indicator file)
 # read in processed WIEWS indicator file, metrics already extracted
 WIEWS_regeneration_summary <- read_csv("../../Data_processing/1_merge_data/2025_07_08/WIEWS_indicator_processed.csv")
-# save results to Drive location
-write.csv(WIEWS_regeneration_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/WIEWS_regeneration_summary.csv", row.names = FALSE)
 
 # 18. PDCI metric
 # Source and Run function to calculate PDCI
@@ -315,8 +266,6 @@ pdci_summary <- df %>%
   summarise(
     median_PDCI = median(PDCI, na.rm = TRUE) #median PDCI
   )
-# save results
-write.csv(pdci_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/pdci_summary.csv", row.names = FALSE)
 
 # 19.- 20. PTFTW Metrics; Note implemented in individual script 5_PTFTW_processing_and_metrics.R
 
@@ -336,14 +285,55 @@ gbif_count_summary <- croplist %>%
     total_GBIF_count = sum(GBIF_count_total, na.rm = TRUE),
     .groups = "drop"
   )
-# save results
-write.csv(gbif_count_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/gbif_count_summary.csv", row.names = FALSE)
 
 # 22. Characterization and Evaluation datasets
 source("Functions/Process_char_eval.R")
 # call location of folders with char and eval datasets extracted from Genesys
 char_eval_summary <- summarize_char_eval('../../Data/Genesys/Characterization_and_evaluation_datasets')
-# save results
-write.csv(char_eval_summary, "../../Data_processing/4_Estimate_metrics/2025_07_14/char_eval_summary.csv", row.names = FALSE)
+
+
+# --------- COMPILE ALL METRICS INTO ONE LIST ---------
+library(openxlsx)
+
+# Collect all metrics into a list
+metrics_list <- list(
+  accession_by_crop_strategy       = accession_by_crop_strategy,
+  accession_by_source              = accession_by_source,
+  unique_institutions              = unique_institutions,
+  cwr_metric                       = cwr_metric,
+  weedy_metric                     = weedy_metric,
+  landrace_metric                  = landrace_metric,
+  breeding_metric                  = breeding_metric,
+  improved_metric                  = improved_metric,
+  othervar_metric                  = othervar_metric,
+  no_SAMPSTAT_metric               = no_SAMPSTAT_metric,
+  unique_taxa                      = unique_taxa,
+  country_count                    = country_count,
+  primary_region_metric            = primary_region_metric,
+  diversity_regions_metric         = diversity_regions_metric,
+  accessions_by_org_type           = accessions_by_org_type,
+  mls_by_orgtype                   = mls_by_orgtype,
+  annex1_count                     = annex1_count,
+  annex1_perc                      = annex1_perc,
+  storage_summary                  = storage_summary,
+  storage_term_summary             = storage_term_summary,
+  sd_outcountry_metric             = sd_outcountry_metric,
+  SGSV_dupl_metric                 = SGSV_dupl_metric,
+  GLIS_dois_count                  = GLIS_dois_count,
+  GLIS_MLS_count                   = GLIS_MLS_count,
+  institution_accessions_summary  = institution_accessions_summary,
+  BGCI_taxa_count                  = BGCI_taxa_count,
+  BGCI_inst_count                  = BGCI_inst_count,
+  WIEWS_regeneration_summary       = WIEWS_regeneration_summary,
+  pdci_summary                     = pdci_summary,
+  gbif_count_summary               = gbif_count_summary,
+  char_eval_summary                = char_eval_summary
+)
+
+# remove list of taxa, causes file save issues; 'unique_taxa' column from uniqe_taxa summary
+metrics_list$unique_taxa <- metrics_list$unique_taxa %>% select(-unique_taxa)
+
+# Save all metrics to excel file with each sheet as different metric
+write.xlsx(metrics_list, file = "../../Data_processing/4_Estimate_metrics/2025_07_15/all_metrics_summary.xlsx")
 
 ############ End of Script ############
