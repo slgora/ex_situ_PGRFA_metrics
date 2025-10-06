dca7ca44ac567a6c2cefc5da04a1d5ab9802e9ce
Sarah Gora <sarahluciagora@gmail.com>
2025-10-06 16:37:46 -0600

    Update to format numbers with commas for values >1,000 and round % columns to 1 decimal place

--- Functions/Generate_results_table2.R
+++ Functions/Generate_results_table2.R
@@ -22,12 +22,12 @@ generate_table2 <- function(institution_accessions_summary, storage_30_or_40_met
   
   vegetative_crops <- c("Aroids", "Breadfruit", "Cassava", "Strawberry", "Sweetpotato")
   
-  # Helper: Format integers with commas if > 10,000
+  # Helper: Format integers with commas if > 1,000
   format_int <- function(x) {
     num <- suppressWarnings(as.numeric(x))
     out <- as.character(num)
     out[is.na(num)] <- NA_character_
-    big <- !is.na(num) & num > 1e4
+    big <- !is.na(num) & num >= 1e3
     out[big] <- format(
       num[big],
       big.mark    = ",",
@@ -37,7 +37,7 @@ generate_table2 <- function(institution_accessions_summary, storage_30_or_40_met
     out
   }
   
-  # Helper: Format storage strings, adding comma to leading number if >10,000
+  # Helper: Format storage strings, adding comma to leading number if >1,000
   format_storage_string <- function(x) {
     sapply(x, function(val) {
       if (is.na(val) || val == "") return(NA_character_)
@@ -46,7 +46,7 @@ generate_table2 <- function(institution_accessions_summary, storage_30_or_40_met
       if (length(regmatch) != 3) return(val) # No match, return original
       num <- suppressWarnings(as.numeric(gsub(",", "", regmatch[2])))
       rest <- regmatch[3]
-      if (!is.na(num) & num > 1e4) {
+      if (!is.na(num) & num >= 1e3) {
         num_str <- format(num, big.mark = ",", scientific = FALSE, trim = TRUE)
         paste0(num_str, rest)
       } else {
@@ -140,8 +140,8 @@ generate_table2 <- function(institution_accessions_summary, storage_30_or_40_met
       top_df <- top_df %>%
         mutate(
           `Number of accessions` = format_int(`Number of accessions`),
-          `Percent of total` = sprintf("%.2f%%", as.numeric(`Percent of total`)),
-          `Cumulative percent` = sprintf("%.2f%%", as.numeric(`Cumulative percent`)),
+          `Percent of total` = sprintf("%.1f%%", as.numeric(`Percent of total`)),
+          `Cumulative percent` = sprintf("%.1f%%", as.numeric(`Cumulative percent`)),
           `Number of accessions included in MLS (from GLIS)` = format_int(`Number of accessions included in MLS (from GLIS)`),
           `Number of accessions included in MLS (from genebank collections databases)` = format_int(`Number of accessions included in MLS (from genebank collections databases)`),
           !!storage_label := storage_format(.data[[storage_label]])
