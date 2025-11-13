2fb1c99fee9cbf6d55892bf29d28ae1951fef338
Sarah Gora <sarahluciagora@gmail.com>
2025-11-13 16:55:36 -0700

    Added botanic garden taxa standardization; added space btwn ".&" in Standardized_taxa
    Updated file paths for botanic garden only standardization tables and output datasets to reflect new dates. Added additional data cleaning step for PG_recommendations field to add space between ".&" in author names

--- 3_Post_taxa_standardization_processing.R
+++ 3_Post_taxa_standardization_processing.R
@@ -1,5 +1,5 @@
 ######## use conversion table with validated taxa for taxa standardisation, 
-## do the processing that comes after standardization,as these depend on the correct genus and species #########
+## do the processing that comes after standardization, as these depend on the correct genus and species #########
 
 ###### 1. Load datasets with taxa to be standardized using conversion table  #######################
 crops <- read_excel("../../Data_processing/Support_files/GCCS_selected_crops/croplist_PG.xlsx")
@@ -19,27 +19,44 @@ combined_df$fullTaxa2 <- trimws(
        )
   )
 
-#### add standardized_taxa column here when the manual vetting is complete and standardization_table becomes available, code to be tested#######
-#load standardization table
-table_standardization <- read_excel("../../Data_processing/Support_files/Taxa_standardization/standardization_table_WFO_GRIN_2025_07_01.xlsx")
+#Load standardization table of taxa in genebanks and botanic gardens
+table_standardization <- read_excel("../../GCCSmetricsI/Data_processing/Support_files/Taxa_standardization/standardization_table_WFO_GRIN_2025_08_22.xlsx")
+#Load standardization table of taxa in botanic gardens only
+table_standardization_bg <- read_excel("../../GCCS metrics project shared folder/GCCSmetricsI/Data_processing/Support_files/Taxa_standardization/BGCI_bg_taxa_excludeforages_standardization_table_WFO_GRIN_2025_11_07.xlsx")
 
-# Replace tabs with space in input_name
+#Replace tabs with space in input_name
 table_standardization$input_name <- gsub("\t", " ", table_standardization$input_name)
+table_standardization_bgci$input_name <- gsub("\t", " ", table_standardization_bgci$input_name)
 
 #Replace multiple spaces with one space 
 table_standardization$input_name <- gsub("\\s+", " ", table_standardization$input_name)
+table_standardization_bgci$input_name <- gsub("\\s+", " ", table_standardization_bgci$input_name)
 
 #Add a space after period, "." between words in PG_recommendation
 table_standardization$PG_recommendation <- gsub("(?<=[a-zA-Z])\\.(?=[a-zA-Z])", ". ", table_standardization$PG_recommendation, perl = TRUE)
+table_standardization_bgci$PG_recommendation <- gsub("(?<=[a-zA-Z])\\.(?=[a-zA-Z])", ". ", table_standardization_bgci$PG_recommendation, perl = TRUE)
+
+#Add a space after period, "." before "&" symbol in PG_recommendation
+table_standardization$PG_recommendation <- gsub("\\.\\s*&", ". &", as.character(table_standardization$PG_recommendation), perl = TRUE)
+table_standardization_bgci$PG_recommendation <- gsub("\\.\\s*&", ". &", as.character(table_standardization_bgci$PG_recommendation), perl = TRUE)
 
 # Create named vector: input_name -> PG_recommendation
 standardization_table <- setNames(table_standardization$PG_recommendation, table_standardization$input_name)
+standardization_table_bgci <- setNames(table_standardization_bgci$PG_recommendation, table_standardization_bgci$input_name)
 
+#standardize taxa in genebanks
 combined_df$Standardized_taxa = NA
 combined_df <- combined_df %>%
     mutate(Standardized_taxa = ifelse(is.na(Standardized_taxa), standardization_table[fullTaxa2], Standardized_taxa))
 
+#standardize taxa only in botanic gardens
 BGCI_processed$Standardized_taxa = NA
+BGCI_processed <- BGCI_processed %>% 
+  mutate( mapped = standardization_table_bgci[fullTaxa], 
+          mapped = if_else(mapped == fullTaxa, NA_character_, mapped), 
+          Standardized_taxa = coalesce(Standardized_taxa, mapped) ) %>% 
+  select(-mapped)
+#standardize taxa in both genebanks and botanic gardens
 BGCI_processed <- BGCI_processed %>%
   mutate(Standardized_taxa = ifelse(is.na(Standardized_taxa), standardization_table[fullTaxa], Standardized_taxa))
 
@@ -89,7 +106,7 @@ combined_df = assign_annex1status(combined_df, standardize_taxa = 'Standardized_
 
 
 ##### save resulting datasets in folder: ../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/
-write.csv(combined_df, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_07_18/combined_df.csv', row.names = FALSE)
+write.csv(combined_df, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_11_13/combined_df.csv', row.names = FALSE)
 write.csv(SGSV_allcrops, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_07_07/SGSV_processed.csv', row.names = FALSE)
 write.csv(GLIS_processed, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_07_07/GLIS_processed.csv', row.names = FALSE)
-write.csv(BGCI_processed, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_07_07/BGCI_processed.csv', row.names = FALSE)
+write.csv(BGCI_processed, '../../Data_processing/3_post_taxa_standardization_processing/Resulting_datasets/2025_11_13/BGCI_processed.csv', row.names = FALSE)
